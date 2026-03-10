c654b5049d739fad5132f3daafbb207b0b2b4c8a
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2026-03-10 10:20:54 +0300

    Completed Fetch Products Detail and Product Detail setup

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,57 @@
+import BreadCrumbs from "@/components/single-product/BreadCrumbs";
+import { fetchSingleProduct } from "@/utils/actions";
+import Image from "next/image";
+import { formatCurrency } from "@/utils/format";
+import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
+import AddToCart from "@/components/single-product/AddToCart";
+import ProductRating from "@/components/single-product/ProductRating";
+import { redirect } from "next/navigation";
+
+async function SingleProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolvedParams = await params;
+  if (!resolvedParams.id) {
+    redirect("/products");
+  }
+
+  const product = await fetchSingleProduct(resolvedParams.id);
+  const { name, company, image, description, price } = product;
+  const dollarsAmount = formatCurrency(price);
+  return (
+    <section>
+      <BreadCrumbs name={product.name} />
+      <div className="mt-16 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/* IMAGE FIRST COL */}
+        <div className="relative h-full">
+          <Image
+            src={image}
+            alt={name}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority
+            className="w-full rounded object-cover"
+          />
+        </div>
+        {/* PRODUCT INFO SECOND COL */}
+        <div>
+          <div className="flex gap-x-8 items-center">
+            <h1 className="capitalize text-3xl font-bold">{name}</h1>
+            <FavoriteToggleButton productId={resolvedParams.id} />
+          </div>
+          <ProductRating productId={resolvedParams.id} />
+          <h4 className="text-xl mt-2">{company}</h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
+            {dollarsAmount}
+          </p>
+          <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
+          <AddToCart productId={resolvedParams.id} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SingleProductPage;
